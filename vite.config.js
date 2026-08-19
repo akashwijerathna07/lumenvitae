b86311bf939e92717b1d0ec32d20fd4f16469056
Akash Wijerathna <akashwijerathna144@gmail.com>
2026-08-19 09:01:05 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
 
-    base: "/lumen-vitae/",
-
-    server: {
-        host: true,
-
-        // watch: {
-        //   usePolling: true,
-        //   interval: 100
-        // }
-    }
+  server: {
+    host: true,
+    // watch: {
+    //   usePolling: true,
+    //   interval: 100
+    // }
+  }
 });
