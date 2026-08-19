3a81c88abe9c85908d0668c863cbc1248859eeee
Akash Wijerathna <akashwijerathna144@gmail.com>
2026-08-19 09:40:15 +0530

    Fix Pages import casing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Home from "./pages/Home/Home.jsx";
-import About from "./pages/About/About.jsx";
-import Products from "./pages/Products/Products.jsx";
-import Partner from "./pages/Partner/Partner.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import About from "./Pages/About/About.jsx";
+import Products from "./Pages/Products/Products.jsx";
+import Partner from "./Pages/Partner/Partner.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Contact from "./Pages/Contact/Contact.jsx";
-import SingleProduct from "./pages/SingleProduct/SingleProduct.jsx";
+import SingleProduct from "./Pages/SingleProduct/SingleProduct.jsx";
 
 function App() {
     return (
